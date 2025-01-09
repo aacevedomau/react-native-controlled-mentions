c45fbe97cd0dcfb4f1c7ea5e1305e45d380a4de2
Cristian Romero <cristianroacosta@gmail.com>
2025-01-09 17:07:38 -0500

    Remove dist folder from gitignore

---
+++ dist/components/mention-input.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { MentionInputProps } from '../types';
+declare const MentionInput: ({ value, onChange, partTypes, inputRef: propInputRef, containerStyle, onSelectionChange, renderListSuggestions, renderListSelection, ...textInputProps }: MentionInputProps) => JSX.Element;
+export { MentionInput };
